c7b62990504af597db6d2ff55ab65e62e4388511
程天聪 <tc.cheng@ugoodtech.com>
2015-11-24 17:20:51 +0800

    commit
    commit

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Alert show in the top of the superview.
 
 
-![](https://github.com/ctc1991/TCTopAlertView/blob/master/Example/demo_show.gif)
+![]()
--- TCLogJsonDemo/TCLogJsonDemo/main.m
+++ TCLogJsonDemo/TCLogJsonDemo/main.m
@@ -12,12 +12,10 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSURL *URL = [NSURL URLWithString:@"http://v.youku.com/player/getPlayList/VideoIDS/XMTM5MDg3ODYyOA"];
+        NSURL *URL = [NSURL URLWithString:@"http://www.zns.link:8083/v1/weather?area=Minghang&date=2015-10-09&days=10"];
         NSData *data = [NSData dataWithContentsOfURL:URL];
         NSString *string = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
         NSLog(@"%@",string);
-        
-        
     }
     return 0;
 }
